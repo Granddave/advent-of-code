cbe5086b2fed7ebc8cfa071aa0f0b62ad45dd28d
David Isaksson <davidisaksson93@gmail.com>
2023-12-01 21:21:08 +0100

    2023-12-01

--- 2023/01/src/bin/part1.rs
+++ 2023/01/src/bin/part1.rs
@@ -9,7 +9,16 @@ fn main() -> Result<()> {
 }
 
 fn solve(input: &str) -> Result<String> {
-    Ok(input.to_string())
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let first_number = line.chars().find(|c| c.is_digit(10)).expect("first");
+        let last_number = line.chars().rev().find(|c| c.is_digit(10)).expect("last");
+        let combined_number = format!("{}{}", first_number, last_number);
+        sum += combined_number.parse::<i32>()?;
+    }
+
+    Ok(format!("{}", sum))
 }
 
 #[cfg(test)]
@@ -20,8 +29,11 @@ mod tests {
 
     #[test]
     fn test() -> anyhow::Result<()> {
-        let input = "";
-        let result = "".to_string();
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let result = "142".to_string();
         assert!(solve(input).unwrap() == result);
 
         Ok(())
