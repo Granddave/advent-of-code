25fd4c9d0e94624529c8ed65431fde3ca99fb64b
David Isaksson <davidisaksson93@gmail.com>
2023-12-03 16:20:04 +0100

    2023-12-03 part 1 done

--- 2023/03/src/bin/part1.rs
+++ 2023/03/src/bin/part1.rs
@@ -9,17 +9,17 @@ fn main() -> Result<()> {
 }
 
 fn is_symbol(ch: char) -> bool {
-    !ch.is_numeric() && ch != '.' && ch != '\n'
+    if !ch.is_numeric() && ch != '.' && ch != '\n' {
+        // println!("{}", ch);
+        return true;
+    }
+    false
 }
 
 fn is_part_number(line_ix: i32, start_ix: i32, end_ix: i32, symbols: &Vec<(i32, i32)>) -> bool {
-    // eprintln!("--");
-    for symbol in symbols {
-        let around_columns = symbol.0 >= start_ix - 1 && symbol.0 <= end_ix + 1;
-        let around_line = symbol.1 >= line_ix - 1 && symbol.1 <= line_ix + 1;
-        // eprintln!("x: {}, y: {}", symbol.0, symbol.1);
-        // eprintln!("l: {}, s: {}, e:{}", line_ix, start_ix, end_ix);
-        // eprintln!("{}, {}", around_columns, around_line);
+    for &(x, y) in symbols {
+        let around_columns = x >= start_ix - 1 && x <= end_ix + 1;
+        let around_line = y >= line_ix - 1 && y <= line_ix + 1;
         if around_columns && around_line {
             return true;
         }
@@ -43,24 +43,27 @@ fn solve(input: &str) -> Result<String> {
         let mut start_ix: Option<usize> = None;
         let mut end_ix: Option<usize> = None;
         for (ix, ch) in line.chars().enumerate() {
+            let is_last_char = ix == line.len() - 1;
             if ch.is_numeric() {
                 if start_ix.is_none() {
                     start_ix = Some(ix);
                 }
                 end_ix = Some(ix)
-            } else if !ch.is_numeric() && end_ix.is_some() {
-                // Check for symbols
+            }
+            if !ch.is_numeric() && end_ix.is_some() || is_last_char && end_ix.is_some() {
                 if is_part_number(
                     line_ix as i32,
                     start_ix.unwrap() as i32,
                     end_ix.unwrap() as i32,
                     &symbols,
                 ) {
-                    // eprintln!("{}", line[start_ix.unwrap()..end_ix.unwrap()].to_string());
-                    sum += line[start_ix.unwrap()..end_ix.unwrap() + 1].parse::<i32>()?;
+                    sum += line
+                        .get(start_ix.unwrap()..end_ix.unwrap() + 1)
+                        .expect("get slice")
+                        .parse::<i32>()
+                        .expect("parse");
                 }
 
-                // Reset start and end
                 start_ix = None;
                 end_ix = None;
             }
@@ -81,23 +84,23 @@ mod tests {
         let tests = vec![
             (
                 "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..",
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..",
                 "4361".to_string(),
             ),
             (
                 "...........
-...........
-...%.......
-123........",
-                "123".to_string(),
+..........%
+..........3
+...........",
+                "3".to_string(),
             ),
         ];
 
